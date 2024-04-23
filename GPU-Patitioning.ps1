0bb23fe3291a5d03e56c34057b9183dc72319483
Yazrog <84290128+Yazrog@users.noreply.github.com>
2024-04-23 21:37:31 +0200

    [FIX] add var

--- GPU-Patitioning.ps1
+++ GPU-Patitioning.ps1
@@ -5,9 +5,9 @@ Param (
 $vm = $VMName
 Add-VMGpuPartitionAdapter -VMName $vm
 if (Get-VMGpuPartitionAdapter -VMName $vm -ErrorAction SilentlyContinue) {
-   Remove-VMGpuPartitionAdapter -VMName $vm
+    Remove-VMGpuPartitionAdapter -VMName $vm
 }
-Set-VM -GuestControlledCacheTypes $true -VMName $vm
-Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $vm
-Set-VM -HighMemoryMappedIoSpace 32Gb -VMName $vm
-Add-VMGpuPartitionAdapter -VMName $vm
+Set-VM -GuestControlledCacheTypes $true -VMName $VMName
+Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $VMName
+Set-VM -HighMemoryMappedIoSpace 32Gb -VMName $VMName
+Add-VMGpuPartitionAdapter -VMName $VMName
